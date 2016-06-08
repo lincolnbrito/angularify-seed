244a8fc70dd9a9cfbf200019c7b0028489d60dcd
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-06-08 14:52:42 -0300

    directive

--- app/notes/directives/notes-directive-list-items.js
+++ app/notes/directives/notes-directive-list-items.js
@@ -3,7 +3,7 @@ angular.module('spa.notes.directives').controller('NotesDirectiveListItemsCtrl',
     console.log("TESTE");
 }]);
 
-var directive = angular.module('spa.notes.directives').directive('diretiva', function(){
+var directive = angular.module('spa.notes.directives').directive('notesItems', function(){
     return {
         templateUrl: 'views/notes/directives/notes-directive-list-items.html',
         controller: 'NotesDirectiveListItemsCtrl',
